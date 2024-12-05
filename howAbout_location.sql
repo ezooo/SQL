6f44bed9288aefe6344ee2fbd8ce6587224bb0ad
cho <choeee4@gmail.com>
2024-12-05 17:54:25 +0900

    location

---
+++ howAbout_location.sql
@@ -0,0 +1,21 @@
+use howabout;
+
+create table location
+(
+	 data_title varchar(20),
+	 user_address varchar(50),
+	 lattitude varchar(50),
+	 logitude varchar(50),
+	 insttnm varchar(20),
+	 category_name1 varchar(20),
+	 category_name2 varchar(20),
+	 data_content text,
+	 telno varchar(20),
+	 fileurl1 text,
+	 fileurl2 text,
+	 fileurl3 text,
+	 fileurl4 text,
+	 fileurl5 text
+);
+
+select * from location;
